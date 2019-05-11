50ae1a637d8cd379c46d6a66a93fbb824d5766d9
Stoian Cristian <stoianurzicacristian@gmail.com>
2019-05-11 07:45:19 -0700

    Baza de date populata

---
+++ SQLScripts/dbo.Elevi.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Elevi] (
+    [nume]          VARCHAR (50)  NOT NULL,
+    [data_nasterii] DATE          NOT NULL,
+    [id_clasa]      INT           NOT NULL,
+    [user]          VARCHAR (255) NOT NULL,
+    [password]      VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([user]),
+    CONSTRAINT [FK_elev_clasa] FOREIGN KEY ([id_clasa]) REFERENCES [dbo].[Clase]([id])
+);
+
