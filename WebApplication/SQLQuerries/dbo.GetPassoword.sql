8de6dc8a266144df9234eb0f6ea5ce8b47b9b940
Stoian Cristian <stoianurzicacristian@gmail.com>
2019-05-12 11:38:41 -0700

    Password Request Done

---
+++ WebApplication/SQLQuerries/dbo.GetPassoword.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetPassoword]
+	@functie text,
+	@username varchar(255) 
+AS
+	IF @functie LIKE 'elev'
+		BEGIN
+		SELECT Elevi.password
+		FROM Elevi
+		WHERE Elevi.username LIKE @username
+		END
+	ELSE
+		BEGIN
+		SELECT Profesori.password
+		FROM Profesori
+		WHERE Profesori.username LIKE @username
+		END
--- WebApplication/WebAppEzHighSchool/WebService.asmx.cs
+++ WebApplication/WebAppEzHighSchool/WebService.asmx.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Services;
 
@@ -24,17 +26,52 @@ namespace WebAppEzHighSchool
 
         private SqlConnection con = new SqlConnection();
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
 
         public WebService()
         {
-            con.ConnectionString = connectionString;
-            con.Open();
+            try
+            {
+                con.ConnectionString = connectionString;
+                con.Open();
+                AllocConsole();
+            }
+            catch (SqlException ex)
+            {
+                string exception = ex.GetType().ToString();
+            }
         }
-
+       
         [WebMethod]
         public string HelloWorld()
         {
             return "Hello World";
+        }
+
+        [WebMethod]
+        public DataSet getPassword(string functie, string user)
+        {
+            try
+            {
+                Debug.WriteLine("Functie: " + functie + " User: "+ user);
+                SqlCommand getPasswordCommand = new SqlCommand("GetPassword", con);
+                getPasswordCommand.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter passowrdAdapter;
+                DataSet passwordDataSet = new DataSet();
+                getPasswordCommand.Parameters.AddWithValue("@functie", functie);
+                getPasswordCommand.Parameters.AddWithValue("@user", user);
+                passowrdAdapter = new SqlDataAdapter(getPasswordCommand);
+                passowrdAdapter.Fill(passwordDataSet);
+                return passwordDataSet;
+            }
+            catch(SqlException ex)
+            {
+                string exception = ex.GetType().ToString();
+            }
+
+            return null;
         }
     }
 }
