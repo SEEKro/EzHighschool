11c346e9660087d9730f88bd3243e1c2ea898b7f
Stoian Cristian <stoianurzicacristian@gmail.com>
2019-05-15 07:00:14 -0700

    Save

--- WebApplication/WebAppEzHighSchool/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAppEzHighSchool.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WebApplication/WebAppEzHighSchool/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAppEzHighSchool.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
