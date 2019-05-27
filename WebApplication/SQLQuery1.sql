2b769b806fbd94e965e92128778cb5a9cb99df91
Stoian Cristian <stoianurzicacristian@gmail.com>
2019-05-27 06:01:51 -0700

    Simplified backend

---
+++ WebApplication/SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO Tricouri(nume, firma, pret, img)
+VALUES('Tricou4', 'Firma2', 55.00, (SELECT * FROM OPENROWSET(BULK N'C:\Users\Cristian\Desktop\Tricouri\t1.jpg', SINGLE_BLOB)as img))
+GO
+
+INSERT INTO Tricouri(nume, firma, pret, img)
+VALUES('Tricou4', 'Firma2', 54.99, (SELECT * FROM OPENROWSET(BULK N'C:\Users\Cristian\Desktop\Tricouri\t2.jpg', SINGLE_BLOB)as img))
+GO
+
+INSERT INTO Tricouri(nume, firma, pret, img)
+VALUES('Tricou4', 'Firma2', 60.99, (SELECT * FROM OPENROWSET(BULK N'C:\Users\Cristian\Desktop\Tricouri\t3.jpg', SINGLE_BLOB)as img))
+GO
+
+INSERT INTO Tricouri(nume, firma, pret, img)
+VALUES('Tricou4', 'Firma2', 100.99, (SELECT * FROM OPENROWSET(BULK N'C:\Users\Cristian\Desktop\Tricouri\t4.jpg', SINGLE_BLOB)as img))
+GO
--- WebApplication/WebAppEzHighSchool/WebService.asmx.cs
+++ WebApplication/WebAppEzHighSchool/WebService.asmx.cs
@@ -50,196 +50,138 @@ namespace WebAppEzHighSchool
             return "Hello World";
         }
 
-        [WebMethod]
-        public DataSet getPassword(string functie, string user)
+        private DataSet makeGetRequest(String procedureName, List<string> parametters, List<Object> effectiveParametters)
         {
             try
             {
-                Debug.WriteLine("getPassoword with #Functie: " + functie + " #User: " + user);
-                SqlCommand getPasswordCommand = new SqlCommand("GetPassword", con);
-                DataSet passwordDataSet = new DataSet();
-                SqlDataAdapter passowrdAdapter;
-                getPasswordCommand.CommandType = CommandType.StoredProcedure;
-                getPasswordCommand.Parameters.AddWithValue("@functie", functie);
-                getPasswordCommand.Parameters.AddWithValue("@username", user);
-                passowrdAdapter = new SqlDataAdapter(getPasswordCommand);
-                passowrdAdapter.Fill(passwordDataSet);
-                return passwordDataSet;
+                SqlCommand getCommand = new SqlCommand(procedureName, con);
+                DataSet getDataSet = new DataSet();
+                SqlDataAdapter getAdapter;
+                getCommand.CommandType = CommandType.StoredProcedure;
+                int index = 0;
+                foreach(string parametter in parametters)
+                {
+                    getCommand.Parameters.AddWithValue(parametter, effectiveParametters.ElementAt(index));
+                    index++;
+                }
+                getAdapter = new SqlDataAdapter(getCommand);
+                getAdapter.Fill(getDataSet);
+                return getDataSet;
             }
-            catch (SqlException ex)
+            catch(SqlException ex)
             {
-                string exception = ex.GetType().ToString();
+                string err = ex.GetType().ToString();
             }
             return null;
         }
 
-        [WebMethod]
-        public DataSet getPersonalInfo(string functie, string user)
+        private int makePostRequest(String procedureName, List<string> parametters, List<Object> effectiveParametters)
         {
             try
             {
-                Debug.WriteLine("getPersonalInfo with #Functie: " + functie + " #User: " + user);
-                SqlCommand getInfoCommand = new SqlCommand("GetPersonalInfo", con);
-                DataSet infoDataSet = new DataSet();
-                SqlDataAdapter passowrdAdapter;
-                getInfoCommand.CommandType = CommandType.StoredProcedure;
-                getInfoCommand.Parameters.AddWithValue("@functie", functie);
-                getInfoCommand.Parameters.AddWithValue("@username", user);
-                passowrdAdapter = new SqlDataAdapter(getInfoCommand);
-                passowrdAdapter.Fill(infoDataSet);
-                return infoDataSet;
+                SqlCommand getCommand = new SqlCommand(procedureName, con);
+                getCommand.CommandType = CommandType.StoredProcedure;
+                int index = 0;
+                foreach (string parametter in parametters)
+                {
+                    getCommand.Parameters.AddWithValue(parametter, effectiveParametters.ElementAt(index));
+                    index++;
+                }
+                return getCommand.ExecuteNonQuery();
             }
             catch (SqlException ex)
             {
-                string exception = ex.GetType().ToString();
+                string err = ex.GetType().ToString();
             }
-            return null;
+            return 0;
+        }
+
+        [WebMethod]
+        public DataSet getPassword(string functie, string user)
+        {
+            return makeGetRequest("GetPassword", 
+                                  new List<string>() { "@functie", "@username" },
+                                  new List<Object>() { functie, user});
+            }
+
+        [WebMethod]
+        public DataSet getPersonalInfo(string functie, string user)
+        {
+            return makeGetRequest("GetPersonalInfo",
+                                  new List<string>() { "@functie", "@username" },
+                                  new List<Object>() { functie, user });
         }
 
         [WebMethod]
         public virtual DataSet getGrades()
         {
-            try
-            {
-                Debug.WriteLine("getGrades");
-                SqlCommand getAllGradesCommand = new SqlCommand("GetAllGrades", con);
-                DataSet gradesDataSet = new DataSet();
-                SqlDataAdapter gradesAdapter;
-                getAllGradesCommand.CommandType = CommandType.StoredProcedure;
-                gradesAdapter = new SqlDataAdapter(getAllGradesCommand);
-                gradesAdapter.Fill(gradesDataSet);
-                return gradesDataSet;
-            }
-            catch (SqlException ex)
-            {
-                string exception = ex.GetType().ToString();
-            }
-            return null;
+            return makeGetRequest("GetAllGrades",
+                                  new List<string>() {},
+                                  new List<Object>() {});
         }
 
         [WebMethod(MessageName = "With parametter")]
         public virtual DataSet getGrades(string user)
         {
-            try
-            {
-                Debug.WriteLine("getGrades");
-                SqlCommand getAllGradesCommand = new SqlCommand("GetAllGradesByUser", con);
-                DataSet gradesDataSet = new DataSet();
-                SqlDataAdapter gradesAdapter;
-                getAllGradesCommand.CommandType = CommandType.StoredProcedure;
-                getAllGradesCommand.Parameters.AddWithValue("@username", user);
-                gradesAdapter = new SqlDataAdapter(getAllGradesCommand);
-                gradesAdapter.Fill(gradesDataSet);
-                return gradesDataSet;
-            }
-            catch (SqlException ex)
-            {
-                string exception = ex.GetType().ToString();
-            }
-            return null;
+            return makeGetRequest("GetAllGradesByUser",
+                                  new List<string>() { "@username" },
+                                  new List<Object>() { user });
         }
 
         [WebMethod]
         public DataSet getAbsenteByUser(string user)
         {
-            try
-            {
-                SqlCommand getAbsenteByUserCommand = new SqlCommand("GetAbesnteByUser", con);
-                DataSet abesenteDataSet = new DataSet();
-                SqlDataAdapter absenteAdapter;
-                getAbsenteByUserCommand.CommandType = CommandType.StoredProcedure;
-                getAbsenteByUserCommand.Parameters.AddWithValue("@username", user);
-                absenteAdapter = new SqlDataAdapter(getAbsenteByUserCommand);
-                absenteAdapter.Fill(abesenteDataSet);
-                return abesenteDataSet;
-            }
-            catch (SqlException ex)
-            {
-                string exception = ex.GetType().ToString();
-            }
-
-            return null;
+            return makeGetRequest("GetAbesnteByUser",
+                                  new List<string>() { "@username" },
+                                  new List<Object>() { user });
         }
 
         [WebMethod]
         public int insertGrade(string user_profesor, string user_elev, string nume_materie, int nota, string data)
         {
-            try
-            {
-                SqlCommand insertGradeCommand = new SqlCommand("InsertGrade", con);
-                insertGradeCommand.CommandType = CommandType.StoredProcedure;
-                insertGradeCommand.Parameters.AddWithValue("@user_elev", user_elev);
-                insertGradeCommand.Parameters.AddWithValue("@user_profesor", user_profesor);
-                insertGradeCommand.Parameters.AddWithValue("@nota", nota);
-                insertGradeCommand.Parameters.AddWithValue("@data", data);
-                insertGradeCommand.Parameters.AddWithValue("@nume_materie", nume_materie);
-                return insertGradeCommand.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                string exception = ex.GetType().ToString();
-            }
-
-            return 0;
+            return makePostRequest("InsertGrade",
+                                  new List<string>() { "@user_elev", "@user_profesor", "@nota", "@data", "@nume_materie" },
+                                  new List<Object>() { user_elev, user_profesor, nota, data, nume_materie });
         }
 
         [WebMethod]
         public int insertAbsenta(string user_elev, string nume_materie, string data)
         {
-            try
-            {
-                SqlCommand insertAbsenta = new SqlCommand("InsertAbsenta", con);
-                insertAbsenta.CommandType = CommandType.StoredProcedure;
-                insertAbsenta.Parameters.AddWithValue("@user_elev", user_elev);
-                insertAbsenta.Parameters.AddWithValue("@nume_materie", nume_materie);
-                insertAbsenta.Parameters.AddWithValue("@data", data);
-                return insertAbsenta.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                string exception = ex.GetType().ToString();
-            }
-
-            return 0;
+            return makePostRequest("InsertAbsenta",
+                                     new List<string>() { "@user_elev", "@nume_materie", "@data" },
+                                     new List<Object>() { user_elev, nume_materie, data });
         }
 
         [WebMethod]
         public int motivateAbsenta(int id_absenta)
         {
-            try
-            {
-                SqlCommand motivateAbsenta = new SqlCommand("MotivateAbsenta", con);
-                motivateAbsenta.CommandType = CommandType.StoredProcedure;
-                motivateAbsenta.Parameters.AddWithValue("@id_absenta", id_absenta);
-                return motivateAbsenta.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                string exception = ex.GetType().ToString();
-            }
-
-            return 0;
+            return makePostRequest("MotivateAbsenta",
+                                     new List<string>() { "@id_absenta" },
+                                     new List<Object>() { id_absenta });
         }
 
         [WebMethod]
         public DataSet getMateriiByProfUser(string user_profesor)
         {
-            try
-            {
-                SqlCommand getMateriiByUserCommand = new SqlCommand("GetMateriiByUser", con);
-                DataSet materiiDataSet = new DataSet();
-                SqlDataAdapter absenteAdapter;
-                getMateriiByUserCommand.CommandType = CommandType.StoredProcedure;
-                getMateriiByUserCommand.Parameters.AddWithValue("@user_profesor", user_profesor);
-                absenteAdapter = new SqlDataAdapter(getMateriiByUserCommand);
-                absenteAdapter.Fill(materiiDataSet);
-                return materiiDataSet;
+            return makeGetRequest("GetMateriiByUser",
+                                  new List<string>() { "@user_profesor" },
+                                  new List<Object>() { user_profesor });
+        }
+
+        [WebMethod]
+        public DataSet getAbsenteByMaterie(string nume_materie, string user_elev)
+        {
+            return makeGetRequest("GetAbsenteByMaterie",
+                                  new List<string>() { "@user_elev", "@nume_materie" },
+                                  new List<Object>() { nume_materie, user_elev });
             }
-            catch (SqlException ex)
-            {
-                string exception = ex.GetType().ToString();
-            }
-            return null;
+
+        [WebMethod]
+        public DataSet getClase()
+        {
+            return makeGetRequest("GetAllClase",
+                                  new List<string>() {  },
+                                  new List<Object>() {  });
         }
     }
 }
